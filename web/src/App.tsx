5b74d76b1f968a02c82caad9cd0056e9408747bf
mertovich <88425310+mertovich@users.noreply.github.com>
2022-07-26 14:55:03 +0200

    add(SectionTwo)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,31 @@
 import {
-  Text,
   Stack,
 } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import CookiesBar from './ui-library/components/CookiesBar'
 import SectionOne from './ui-library/components/SectionOne'
+import SectionTwo from './ui-library/components/SectionTwo'
 import MainLayout from './ui-library/Layouts/MainLayout'
 import PageLayout from './ui-library/Layouts/PageLayout'
 
 type Props = {}
 
 const App = (props: Props) => {
+
+  useEffect(() => {
+    function handleResize() {
+      window.location.reload()
+    }
+    window.addEventListener('resize', handleResize)
+  }, [])
+
   return (
     <MainLayout>
       <PageLayout>
         <Stack>
-            <SectionOne/>
-            <CookiesBar/>
+          <SectionOne />
+          <SectionTwo/>
+          <CookiesBar />
         </Stack>
       </PageLayout>
     </MainLayout>
---
+++ web/src/ui-library/components/SectionTwo.tsx
@@ -0,0 +1,79 @@
+import {
+  Box,
+  SimpleGrid,
+  VStack,
+  Text,
+} from '@chakra-ui/react'
+import React from 'react'
+import circle from '../../assets/images/circle.svg'
+import Card from './Card'
+import card1 from '../../assets/images/card1.svg'
+import card2 from '../../assets/images/card2.svg'
+import card3 from '../../assets/images/card3.svg'
+import card4 from '../../assets/images/card4.svg'
+import NavBar from './NavBar'
+
+
+type Props = {}
+
+const SectionTwo = (props: Props) => {
+  return (
+    <Box
+      id='2'
+      height='100vh'
+      bgImage={circle}
+      bgRepeat='no-repeat'
+      bgPosition='center'
+      bgSize='100vh'
+    >
+      <NavBar divName='2' />
+      <VStack
+        marginTop='5%'
+        marginBottom='5%'
+      >
+        <Text
+          fontSize='3xl'
+          fontWeight='bold'
+          bgGradient='linear(to-l, grey, white, grey)'
+          bgClip='text'
+        >
+        WIFI Technologie.
+        </Text>
+        <Text
+          color='grey'
+          fontSize='xl'
+        >
+        Unzählige Einsatzmöglichkeiten. Finden Sie Ihre:
+        </Text>
+      </VStack>
+      <SimpleGrid
+        marginLeft='5%'
+        columns={[2, null, 4]}
+        spacing={10}
+      >
+        <Card
+          title='Home'
+          content='Für ein smartes Zuhause.'
+          image={card1}
+        />
+        <Card 
+        title='Business'
+        content='Für ein vernetztes Unternehmen.'
+        image={card2}
+        />
+        <Card 
+        title='Education'
+        content='Für eine digitale Zukunft.'
+        image={card3}
+        />
+        <Card 
+        title='Industrial'
+        content='Für den digitalen Fortschritt.'
+        image={card4}
+        />
+      </SimpleGrid>
+    </Box>
+  )
+}
+
+export default SectionTwo
